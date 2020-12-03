be791443d2b810a608e51f1dddd9b0d6d381ae27
Kevin Diaz <kevindiaz712@gmail.com>
2020-12-03 12:04:17 -0600

    ADD; codigo listo

--- layout/body/Body.js
+++ layout/body/Body.js
@@ -1,44 +1,49 @@
 
+import React, {Component} from 'react';
 import CardResul from "../../pages/components/card1/Results"
 import CardInfo from "../../pages/components/card2/Info"
 import { Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import {cleanAll} from '../src/redux/actions/searching';
+import {cleanAll} from '../../src/redux/actions/searching';
 
-const Body = (props) => (    
+export class Body extends Component{    
 
-    
-
-    <div className="container" style={{paddingTop: 80}}>  
-        <div className="col-md-12">
+    componentWillMount(){
         
-        <div>
-            <Row>
-                <Col md={4}>
-                    <CardResul icon="user" color="warning" header="Usuarios" value={props.totalUsers}/>
-                </Col>
-                <Col md={4}>
-                    <CardResul icon="repo" color="info" header="Repositorios" value={props.totalRepo}/>
-                </Col>
-            </Row>
-        </div>
-        <div style={{paddingTop: 50}}> 
-            <Row>
+        this.props.cleanAll();  
+      }
+    render(){
+        return (<div className="container" style={{paddingTop: 80}}>  
+                <div className="col-md-12">
+                
+                <div>
+                    <Row>
+                        <Col md={4}>
+                            <CardResul icon="user" color="warning" header="Usuarios" value={this.props.totalUsers}/>
+                        </Col>
+                        <Col md={4}>
+                            <CardResul icon="repo" color="info" header="Repositorios" value={this.props.totalRepo}/>
+                        </Col>
+                    </Row>
+                </div>
+                <div style={{paddingTop: 50}}> 
+                    <Row>
 
-            {props.listRepo && props.listRepo.map((data, idx) => {
-                  return <Col key={idx} md={4} style={{paddingBottom: 20}}>
-                            <CardInfo 
-                            info={props.users}
-                            reponame={data.name}
-                            />
-                        </Col> ;
-                })}
-                 
-            </Row>
-        </div>    
-        </div>     
-    </div>       
-  );
+                    {this.props.listRepo && this.props.listRepo.map((data, idx) => {
+                        return <Col key={idx} md={4} style={{paddingBottom: 20}}>
+                                    <CardInfo 
+                                    info={this.props.users}
+                                    reponame={data.name}
+                                    />
+                                </Col> ;
+                        })}
+                        
+                    </Row>
+                </div>    
+                </div>     
+            </div>  
+        ) }  
+}
   
   const mapStateToProps = (state) => {
    return{    
